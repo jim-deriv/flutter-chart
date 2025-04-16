552ecc13b95040b50cbfa9dc0bfb960204a23bda
Jim Daniels Wasswa <jim@regentmarkets.com>
2025-04-16 11:49:30 +0800

    chore: pass labelBackgroundColor as an argument to paintLabelBackground

--- lib/src/deriv_chart/chart/data_visualization/annotations/barriers/horizontal_barrier/horizontal_barrier_painter.dart
+++ lib/src/deriv_chart/chart/data_visualization/annotations/barriers/horizontal_barrier/horizontal_barrier_painter.dart
@@ -180,7 +180,7 @@ class HorizontalBarrierPainter<T extends HorizontalBarrier>
     }
 
     // Label.
-    paintLabelBackground(canvas, labelArea, style.labelShape, _paint);
+    paintLabelBackground(canvas, labelArea, style.labelShape, _paint, style.labelShapeBackgroundColor);
     paintWithTextPainter(
       canvas,
       painter: valuePainter,
@@ -214,12 +214,10 @@ class HorizontalBarrierPainter<T extends HorizontalBarrier>
 
   /// Paints a background based on the given [LabelShape] for the label text.
   void paintLabelBackground(
-      Canvas canvas, Rect rect, LabelShape shape, Paint paint,
+      Canvas canvas, Rect rect, LabelShape shape, Paint paint, Color labelBackgroundColor,
       {double radius = 4}) {
-    final HorizontalBarrierStyle style =
-        series.style as HorizontalBarrierStyle? ?? theme.horizontalBarrierStyle;
 
-    paint.color = style.labelShapeBackgroundColor;
+    paint.color = labelBackgroundColor;
 
     if (shape == LabelShape.rectangle) {
       canvas.drawRRect(
