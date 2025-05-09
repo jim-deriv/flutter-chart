b564d5c78c26b6826a0d7603220f5dbfb0552fa7
ramin-deriv <ramin.vakili@deriv.com>
2025-05-09 10:16:21 +0800

    code cleanup :recycle:

--- lib/src/deriv_chart/interactive_layer/interactive_layer_states/interactive_adding_tool_state.dart
+++ lib/src/deriv_chart/interactive_layer/interactive_layer_states/interactive_adding_tool_state.dart
@@ -9,7 +9,6 @@ import 'package:deriv_chart/src/deriv_chart/chart/data_visualization/drawing_too
 import 'package:deriv_chart/src/deriv_chart/chart/data_visualization/drawing_tools/drawing_data.dart';
 import 'package:deriv_chart/src/deriv_chart/chart/data_visualization/models/animation_info.dart';
 import 'package:deriv_chart/src/deriv_chart/interactive_layer/interactable_drawings/horizontal_line_interactable_drawing.dart';
-import 'package:deriv_chart/src/deriv_chart/interactive_layer/interactive_layer_base.dart';
 import 'package:deriv_chart/src/models/axis_range.dart';
 import 'package:deriv_chart/src/theme/painting_styles/line_style.dart';
 import 'package:flutter/gestures.dart';
